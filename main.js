e4f6fb9ec8d2431e2f6df2f9e8c8bcc5947c23e5
vorisxon <vorisxon.me@gmail.com>
2024-12-19 11:02:00 +0500

    media screen burger apply

--- main.js
+++ main.js
@@ -57,3 +57,41 @@ app.post('/submit', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
+
+
+const hamburger = document.querySelector(".nav__hamburger");
+const linksContainer = document.querySelector(".nav__menu");
+const links = document.querySelectorAll(".nav__menu__link");
+
+hamburger.addEventListener("click", () => {
+  linksContainer.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+window.addEventListener("resize", () => {
+  if (window.matchMedia("(max-width: 550px)").matches) {
+    closeMenu();
+  }
+});
+
+if (window.matchMedia("(max-witdh: 550px").matches) {
+  closeMenu();
+}
+
+function closeMenu() {
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      linksContainer.classList.remove("active");
+      hamburger.classList.remove("active");
+    });
+  });
+}
+// Проверка работы меню
+hamburger.addEventListener("click", () => {
+   if (linksContainer.classList.contains("active")) {
+     console.log("Меню открыто");
+   } else {
+     console.log("Меню закрыто");
+   }
+ });
+ 
