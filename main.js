b98b57d5a2fc958947791f87f7cf598e02424824
vorisxon <vorisxon.me@gmail.com>
2024-12-19 11:53:34 +0500

    media screen burger apply part2

--- main.js
+++ main.js
@@ -59,39 +59,36 @@ app.listen(PORT, () => {
 });
 
 
-const hamburger = document.querySelector(".nav__hamburger");
-const linksContainer = document.querySelector(".nav__menu");
-const links = document.querySelectorAll(".nav__menu__link");
 
+
+
+
+
+
+// Находим элементы
+const hamburger = document.querySelector(".nav__hamburger");
+const menu = document.querySelector(".nav__menu");
+
+// Открытие/закрытие меню
 hamburger.addEventListener("click", () => {
-  linksContainer.classList.toggle("active");
+  menu.classList.toggle("active");
   hamburger.classList.toggle("active");
+//   hamburger.style.display = block();
+//   menu.style.display = block();
 });
 
+// Закрытие меню при клике на ссылку
+document.querySelectorAll(".nav__menu__link a").forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+    hamburger.classList.remove("active");
+  });
+});
+
+// Отслеживаем размер экрана, чтобы скрыть меню при ресайзе
 window.addEventListener("resize", () => {
-  if (window.matchMedia("(max-width: 550px)").matches) {
-    closeMenu();
+  if (window.innerWidth > 768) {
+    menu.classList.remove("active");
+    hamburger.classList.remove("active");
   }
 });
-
-if (window.matchMedia("(max-witdh: 550px").matches) {
-  closeMenu();
-}
-
-function closeMenu() {
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      linksContainer.classList.remove("active");
-      hamburger.classList.remove("active");
-    });
-  });
-}
-// Проверка работы меню
-hamburger.addEventListener("click", () => {
-   if (linksContainer.classList.contains("active")) {
-     console.log("Меню открыто");
-   } else {
-     console.log("Меню закрыто");
-   }
- });
- 
